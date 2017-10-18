1ccdc122fbe69c3359053d67eb2b703eac2b2125
adiyamgn <adiyamgn@outlook.com>
2017-10-18 00:05:31 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { EvaluationsComponent } from './evaluations/evaluations.component';
+
+const appRoutes: Routes = [
+  {path: 'evaluations', component: EvaluationsComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EvaluationsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCardModule, 
+    MatMenuModule, 
+    MatToolbarModule, 
+    MatIconModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
